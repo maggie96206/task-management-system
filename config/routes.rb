bebfde3835e4191e2ac7416d8d85829b8fface48
maggie96206 <maggie96206@gmail.com>
2026-02-20 20:19:24 +0800

    feat: implement basic task CRUD with flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "tasks/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "tasks#index" # 設定首頁為任務列表
+  resources :tasks   # 自動產生 CRUD 的 8 個標準網址
 end
