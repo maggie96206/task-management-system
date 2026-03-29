380968169124236f22d5f6ee2981ffc82dea1777
maggie96206 <maggie96206@gmail.com>
2026-03-30 00:50:05 +0800

    Fix style issues and update vulnerable gems

--- db/migrate/20260329100042_add_unique_index_to_tags_name_and_user_id.rb
+++ db/migrate/20260329100042_add_unique_index_to_tags_name_and_user_id.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToTagsNameAndUserId < ActiveRecord::Migration[8.1]
   def change
-    add_index :tags, [:name, :user_id], unique: true
+    add_index :tags, [ :name, :user_id ], unique: true
   end
 end
