4256c0d7b2eb14e90d0647b43a44f47b0d5dd1a2
maggie96206 <maggie96206@gmail.com>
2026-04-20 18:58:34 +0800

    end_at warning flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+
+      # 檢查是否有過期或接近到期的任務
+      urgent_tasks = user.tasks.where("end_at < ?", 24.hours.from_now)
+                               .where(status: [ :pending, :in_progress ])
+
+      if urgent_tasks.any?
+        flash[:warning] = t("sessions.warning", count: urgent_tasks.count)
+      end
+
       redirect_to tasks_path, notice: t("sessions.log_in_success")
+
     else
       flash.now[:alert] = t("sessions.log_in_fail")
       render :new, status: :unprocessable_entity
